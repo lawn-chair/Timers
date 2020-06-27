c771e07e0acaffd091b8825807bcb99db3999b43
Brent Rahn <brent.rahn@gmail.com>
2020-06-27 14:03:19 -0700

    Add TimerDetailView to repo

---
+++ Timers/TimerDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerDetailView.swift
+//  Timers
+//
+//  Created by Brent Rahn on 6/21/20.
+//  Copyright © 2020 Brent Rahn. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerDetailView()
+    }
+}
